317979d3fe4f11c911c612ec6adac504350a28b2
Serhii K <skv.sbkr@gmail.com>
2020-04-19 22:13:38 +0300

    Download CV button, UI and theme changes

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -2,14 +2,14 @@
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "/index.html": "503b465eb3e1062d21a2147fe10a44fc",
-"/main.dart.js": "a9380157c9b7b001c7324f1c4cb95f8f",
+"/main.dart.js": "581d8583ef74849a52ab2dd43ad3d9d7",
 "/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "/manifest.json": "cd6ccf3f008630b01f72cf25d20e5c37",
 "/assets/LICENSE": "e2c33e420a3622ce5140b092c7cf3a85",
-"/assets/AssetManifest.json": "d44c811a168996b2399fb7a06fa14c03",
-"/assets/ww/assets/data.json": "c05b983c56e9f883643bea0cfbbcf6dd",
-"/assets/data.json": "c05b983c56e9f883643bea0cfbbcf6dd",
+"/assets/AssetManifest.json": "5b9d0053e7959c560f129cbfc50f3972",
+"/assets/ios/Flutter/App.framework/flutter_assets/data.json": "6f099153f39e0adc0f2e45156a694b0e",
+"/assets/data.json": "962252b5c5994ec04cca56fb175682f2",
 "/assets/FontManifest.json": "18eda8e36dfa64f14878d07846d6e17f",
 "/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "/assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "d21f791b837673851dd14f7c132ef32e",
